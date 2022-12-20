c9f64a1586960612144e34261dd88a390bb055f0
markkong30 <markkong30@gmail.com>
2022-12-20 00:07:27 +0000

    add emitter limit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import Chat from './pages/Chat/Chat';
@@ -8,8 +8,21 @@ import { ToastContainer } from 'react-toastify';
 import { globalThemes } from './styled.theme';
 import SetAvatar from './pages/Avatar/SetAvatar';
 import { UserInfo } from './context/UserContext';
+import { EventEmitter } from 'events';
 
 function App() {
+	useEffect(() => {
+		const emitter = new EventEmitter();
+
+		// Increase the value of the defaultMaxListeners property to 100
+		emitter.setMaxListeners(100);
+
+		// Add a listener to the emitter
+		emitter.on('myEvent', () => {
+			console.log('myEvent was emitted');
+		});
+	});
+
 	return (
 		<UserInfo>
 			<ThemeProvider theme={globalThemes}>
