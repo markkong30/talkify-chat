646f6dd07eacb10480678507ecd9915327ee68cf
markkong30 <markkong30@gmail.com>
2022-12-20 01:18:57 +0000

    update cors

--- server/index.js
+++ server/index.js
@@ -15,7 +15,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
 	cors({
-		origin: ['http://localhost:3000', 'https://talkify.up.railway.app'],
+		origin: [
+			'http://localhost:3000',
+			'https://talkify.up.railway.app',
+			'https://talkify-chat.netlify.app'
+		],
 		credentials: true
 	})
 );
@@ -42,7 +46,11 @@ const server = httpServer.listen(PORT, () =>
 
 const io = new Server(server, {
 	cors: {
-		origin: ['http://localhost:3000', 'https://talkify.up.railway.app'],
+		origin: [
+			'http://localhost:3000',
+			'https://talkify.up.railway.app',
+			'https://talkify-chat.netlify.app'
+		],
 		credentials: true
 	}
 });
